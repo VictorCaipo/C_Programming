254d4493bd4af0c5dadecbee6704f310061173b6
Victor_Victus <caipoccoa@outlook.com>
2026-04-03 13:03:50 +0200

    dynamically_arrays

--- 25.dynamically_arrays/dynamically_arrays.c
+++ 25.dynamically_arrays/dynamically_arrays.c
@@ -4,40 +4,40 @@
 int main(void){
 
     int n;
-    printf("Cuantos numeros quieres guardar? ");
+    printf("How many numbers do you want to save? ");
     scanf("%d", &n);
 
     //allocate memory
-    int *arr = (int *) malloc(sizeof(int) *n);//int pointer
+    int *arr = (int *) malloc(sizeof(int) *n);
+    //casting void pointer into a int pointer
 
     if(arr == NULL){
-        printf("Error al asignar memoria\n");
+        printf("Error assigning values\n");
         return 1;
     }
 
     //read data
     for(int i=0; i<n ; i++){
-        printf("Numero %d: ", i+1);
+        printf("Assigning number %d: ", i+1);//printing headers
         scanf("%d", &arr[i]);
     }
 
     //print data
-    printf("\nNumeros ingresados:\n");
+    printf("\nAssigned values:\n");
+    //  /əˈsaɪnd/
     for(int i=0; i<n ; i++){
         printf("%d\n", *(arr + i));
     }
-
     //free data
     free(arr);
-
     return 0;
 }
 
 /*
 Dynamic memory array:
-    We can create using malloc function, which allocate memory in
-    the heap. It allocate bytes and return a void pointer, if it
-    doesn't work return a NULL pointer.
+    We can create them using malloc function, which allocate memory in
+    the heap. The malloc function allocate bytes and return a void pointer,
+    if it doesn't work it will return a NULL pointer.
     Sizeof is important because it allows code to be portable
     Calloc y realloc are others handy functions
 */
