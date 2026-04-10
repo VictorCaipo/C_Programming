5af169d84974dbf59e83c351c853c933585bd4ac
Victor_Victus <caipoccoa@outlook.com>
2026-04-10 18:15:50 +0200

    introduction

---
+++ 01.introduction/introduction.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+//A library that allow us to get an input, output from the OS
+
+
+//The OS always run the main function
+int main(void){
+    /*
+    int means that the main function will return an int
+    main is the function name
+    void means that no parameter is needed
+    */
+
+    int age = 25;
+    
+    printf("My age is: %d Yeah I am old.\n", age);
+    
+    return 0;
+}
--- 21.structures/2.structure_en_stack/structure_en_stack.c
+++ 21.structures/2.structure_en_stack/structure_en_stack.c
@@ -1,33 +1,39 @@
-/*
-
-*/
-
 #include <stdio.h>
+
+typedef struct{
+    char id;
+    int valor;
+} mi_structure;
+
+void asinacion_structure(mi_structure str);
+void imprimir_structure(mi_structure str);
 
 
 int main(void){
 
-    typedef struct{
-        char id;
-        int valor;
-    } mi_structure;
-
     mi_structure sensor;
 
-    //valores de la structura
     printf("\nDatos del sensor:\n");
     printf("ID: %c\n", sensor.id);
     printf("Altitud: %d\n", sensor.valor);
-    //C no limpia la memoria
 
-    //size de la structura
     int structure_size = sizeof(sensor);
     printf("El size del structure es: %d\n", structure_size);
-    //uno pensaria que el procesador almacena 5 bytes, pero almacena 8
-    //esto debido a que cuando la direccion es multiplo de 4 es mas facil de leer
     
-
+    asinacion_structure(sensor);
+    imprimir_structure(sensor);
 
     return 0;
 }
 
+void asinacion_structure(mi_structure str){
+    str.id = 'V';
+    str.valor = 10;
+}
+
+
+void imprimir_structure(mi_structure str){
+    printf("\nDatos del sensor:\n");
+    printf("ID: %c\n", str.id);
+    printf("Altitud: %d\n", str.valor);
+}
